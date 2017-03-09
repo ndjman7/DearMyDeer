ac49ed0c931c795184537b34863b91a73bfbb9de
ndjman7 <ndjman7@gmail.com>
2017-03-10 06:01:34 +0900

    Fixed message url

--- information/urls.py
+++ information/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'information'
 urlpatterns = [
     url(r'keyboard$', views.test, name='test'),
-    url(r'messages$', views.test2, name='test2'),
+    url(r'message$', views.test2, name='test2'),
 ]
