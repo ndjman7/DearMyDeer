aae3d4dc7d043c929d152e278931739c77ed5c37
ndjman7 <ndjman7@gmail.com>
2017-03-09 17:54:03 +0900

    settings.py에 중요한 정보 숨김

--- deermydear/settings.py
+++ deermydear/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+CONF_DIR = os.path.join(BASE_DIR, '.conf')
+config = json.loads(open(os.path.join(CONF_DIR, 'django-deploy.json')).read())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-9rg&^f199zkturbkr51u760r3gc(5pus&q+oee_j&$6e12u(2'
+SECRET_KEY = config['DJANGO']['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config['DJANGO']['ALLOWED_HOSTS']
 
 
 # Application definition
