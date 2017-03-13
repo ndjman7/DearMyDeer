b4c8c127aa4c2840059ab44c49b92a83b96a6f3a
ndjman7 <ndjman7@gmail.com>
2017-03-13 20:03:02 +0900

    message API test

--- information/views.py
+++ information/views.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -6,16 +7,38 @@ from rest_framework.decorators import api_view
 def test(request):
     data = {
         'type': 'buttons',
-        'buttons': ["선택 1", "선택 2", "선택 3"]
+        'buttons': ["미래백년관", "밀레니엄관", "학식정보"]
     }
     return Response(data)
 
 
 @api_view(['POST'])
 def test2(request):
-    data = {
-        "message": {
-            "text": "Pando"
+
+    json_data = (request.body).decode('utf-8')
+    received_json_data = json.loads(json_data)
+    content = received_json_data['content']
+    data = dict()
+    if content == "미래백년관":
+        data = {
+            "message": {
+                "text": "미래백년관"
+            }
         }
-    }
+    elif content == "밀레니엄관":
+        data = {
+            "message": {
+                "text": "밀레니엄관"
+            }
+        }
+    elif content == "학식정보":
+        data = {
+            "message": {
+                "text":
+                    "중식 : 11시 00분 ~ 14시 00분\
+                     간식 : 16시 00분 ~ 17시 00분\
+                     석식 : 17시 00분 ~ 18시 30분"
+            }
+        }
+
     return Response(data)
