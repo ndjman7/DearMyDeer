c778b658ac685a6a4835994b7581c6d1024917b1
ndjman7 <ndjman7@gmail.com>
2017-03-15 22:06:18 +0900

    학식 정보 저장할 DeerSchoolFood 모델 구현

--- information/admin.py
+++ information/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import DeerSchoolFood
 
-# Register your models here.
+admin.site.register(DeerSchoolFood)
--- information/models.py
+++ information/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class DeerSchoolFood(models.Model):
+    date = models.DateField(auto_now_add=True, unique=True)
+    future_centennial_hall_food = models.TextField(null=True)
