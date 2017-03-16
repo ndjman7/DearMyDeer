e5ced6ead2274f90a1ffa9c8e570ed1fe4d67cc9
ndjman7 <ndjman7@gmail.com>
2017-03-16 23:45:40 +0900

    학식 모델에 밀레니엄관 정보 추가

--- information/models.py
+++ information/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class DeerSchoolFood(models.Model):
     date = models.DateField(auto_now_add=True, unique=True)
     future_centennial_hall_food = models.TextField(null=True)
+    millennium_hall_food = models.TextField(null=True)
+
