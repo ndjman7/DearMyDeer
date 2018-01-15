4ff69e3333786d1bebb4f4886ab5ebdde2d3f936
pando <ndjman7@gmail.com>
2018-01-16 01:25:02 +0900

    deermydear -> dearmydeer로 수정

--- dearmydeer/settings.py
+++ dearmydeer/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'deermydear.urls'
+ROOT_URLCONF = 'dearmydeer.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'deermydear.wsgi.application'
+WSGI_APPLICATION = 'dearmydeer.wsgi.application'
 
 
 # Database
